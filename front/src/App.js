5c418b974a55264897d2b3b93b1b7ff4c02ea380
Chloe Yeo <yeochloe01@gmail.com>
2024-05-07 13:15:34 +0900

    implemented image slider using react-image-gallery

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import "./assets/css/tStyle.css";
 import Layout from "./layout/Layout";
 import MainPage from "./layout/MainPage";
+import PostWrite from "./pages/PostWrite";
 
 function App() {
   return (
     <Routes>
+      {/* element is where Outlet is */}
       <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />}></Route>
+        <Route path="/postwrite" element={<PostWrite />}></Route>
       </Route>
     </Routes>
   );
--- front/src/layout/Layout.js
+++ front/src/layout/Layout.js
@@ -1,16 +1,33 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ReactImageGallery from "react-image-gallery"; // to create slides
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const Layout = () => {
+  const slideImg = [
+    {
+      original: "./images/slide02.png",
+      description: "image1",
+    },
+    {
+      original: "./images/slide02.png",
+      description: "image2",
+    },
+  ];
   return (
     <>
       <div className="container m-auto">
         <Navbar />
       </div>
 
+      <div>
+        {/* put image inside items */}
+        <ReactImageGallery items={slideImg} showFullscreenButton={false} />
+      </div>
+
       <div className="container m-auto">
-        {/* Outlet for changing pages */}
+        {/* Outlet for changing pages = outlet is where PostWrite body is */}
         <Outlet />
       </div>
       <Footer />
