898600f65825c2e93569c24f0664c139533a643a
Chloe Yeo <yeochloe01@gmail.com>
2024-05-10 12:00:30 +0900

    index error solved and now search working

--- back/src/models/Product.js
+++ back/src/models/Product.js
@@ -1,32 +1,44 @@
 const { Schema, model } = require("mongoose");
 
 const productSchema = new Schema({
-  title: {
-    type: String,
-    maxLength: 30,
-  },
-  description: String,
-  price: {
-    type: Number,
-    default: 0,
-  },
-  images: {
-    type: Array,
-    default: [],
-  },
-  sold: {
-    type: Number,
-    default: 0,
-  },
-  continents: {
-    type: Number,
-    default: 1,
-  },
-  views: {
-    type: Number,
-    default: 0,
-  },
+    title: {
+        type: String,
+        maxLength: 30,
+    },
+    description: String,
+    price: {
+        type: Number,
+        default: 0,
+    },
+    images: {
+        type: Array,
+        default: [],
+    },
+    sold: {
+        type: Number,
+        default: 0,
+    },
+    continents: {
+        type: Number,
+        default: 1,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
 });
+productSchema.index(
+    {
+        title: "text",
+        description: "text",
+    },
+    {
+        weights: {
+            title: 5,
+            description: 1,
+        },
+    }
+);
 const Product = model("product", productSchema);
 
 module.exports = Product;
