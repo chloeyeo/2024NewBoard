af657c89f22620efa79f0070095d45234d2cf93e
Chloe Yeo <yeochloe01@gmail.com>
2024-05-13 14:52:13 +0900

    show image on list

--- front/src/components/CardItem.js
+++ front/src/components/CardItem.js
@@ -1,5 +1,13 @@
 const CardItem = ({ product }) => {
-    return <div className="border py-4 px-2">{product.title}</div>;
+    return (
+        <div className="border py-4 px-2">
+            <img
+                src={`${process.env.REACT_APP_NODE_SERVER_URL}/uploads/${product.images[0]}`}
+                alt="product img"
+            />
+            <div>{product.title}</div>
+        </div>
+    );
 };
 
 export default CardItem;
