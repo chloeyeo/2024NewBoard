fc3ccd1e5d022ac09bbc87307fb53a2005ff52ad
Chloe Yeo <yeochloe01@gmail.com>
2024-05-13 15:13:18 +0900

    image on list update

--- front/src/components/CardItem.js
+++ front/src/components/CardItem.js
@@ -1,10 +1,30 @@
+import styled from "styled-components";
+
 const CardItem = ({ product }) => {
+    const ImageWrap = styled.div`
+        width: 100%;
+        // height: 200px;
+        overflow: hidden;
+        &:hover img {
+            transform: scale(1.2);
+        }
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            transition: 0.3s;
+        }
+    `; // component
     return (
         <div className="border py-4 px-2">
-            <img
-                src={`${process.env.REACT_APP_NODE_SERVER_URL}/uploads/${product.images[0]}`}
-                alt="product img"
-            />
+            {product.images.length > 0 && (
+                <ImageWrap>
+                    <img
+                        src={`${process.env.REACT_APP_NODE_SERVER_URL}/uploads/${product.images[0]}`}
+                        alt="product img"
+                    />
+                </ImageWrap>
+            )}
             <div>{product.title}</div>
         </div>
     );
